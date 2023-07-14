87b66509a30a64a44c4a3fc155984f2cbe425fce
JoseJDC <93933615+JoseJDC@users.noreply.github.com>
2023-07-14 01:07:13 -0600

    Añado el Scanner
    Para capturar datos del usuario

--- src/com/hk/Main.java
+++ src/com/hk/Main.java
@@ -1,10 +1,14 @@
 package com.hk;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		sc.close();
 	}
 
 }
