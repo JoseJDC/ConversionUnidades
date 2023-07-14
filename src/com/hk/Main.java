33b200fa70edfca6e0e901a7a104216dee201e8f
JoseJDC <93933615+JoseJDC@users.noreply.github.com>
2023-07-14 01:41:55 -0600

    Creo lo métodos principales
    Quito el billete de $50 y creo métodos que realizaran los cálculos individualmente

--- src/com/hk/Main.java
+++ src/com/hk/Main.java
@@ -10,7 +10,6 @@ public class Main {
 		
 		double cantidad;
 		int $100;
-		int $50;
 		int $20;
 		int $10;
 		int $5;
@@ -27,12 +26,19 @@ public class Main {
 			System.out.println("1. Calcular cambio");
 			System.out.println("2. Salir");
 			opcion = sc.nextInt();
+			
 			if(opcion == 2) {
 				break;
 			}
 			
 			System.out.println("Introduce la cantidad:");
 			cantidad = sc.nextDouble();
+			
+			$100 = calcularBilletes$100(cantidad);
+			$20 = calcularBilletes$20(cantidad);
+			$10 = calcularBilletes$10(cantidad);
+			$5 = calcularBilletes$5(cantidad);
+			$1 = calcularBilletes$1(cantidad);
 		}
 		
 		
@@ -40,6 +46,31 @@ public class Main {
 		cantidad = sc.nextDouble();
 		
 		sc.close();
+	}
+
+	private static int calcularBilletes$1(double cantidad) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private static int calcularBilletes$5(double cantidad) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private static int calcularBilletes$10(double cantidad) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	static int calcularBilletes$20(double cantidad) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	static int calcularBilletes$100(double cantidad) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
