b8135f2d8c624703bd3ab5a20af568845dc5c86e
JoseJDC <93933615+JoseJDC@users.noreply.github.com>
2023-07-13 22:21:06 -0600

    Comentarios y borro los Sys.out
    Comento los pasos del sistema y los métodos para calcular cada billete

--- src/com/hk/Main.java
+++ src/com/hk/Main.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 public class Main {
 
+	// Cantidad ingresada por el usuario
 	static double cantidad;
+	// Cantidad que disminuye con cada billete que se calcula
 	static double cantidadActual;
 
 	public static void main(String[] args) {
 
+		// Creo el Scanner para capturar entrada de datos
 		Scanner sc = new Scanner(System.in);
 
+		// Inicializo todas las varibles que representa cada billete
 		int $100 = 0;
 		int $20 = 0;
 		int $10 = 0;
@@ -21,6 +25,7 @@ public class Main {
 		int $0_05 = 0;
 		int $0_01 = 0;
 
+		// Variable de usuario
 		int opcion = 0;
 
 		while (true) {
@@ -29,14 +34,17 @@ public class Main {
 			System.out.println("2. Salir");
 			opcion = sc.nextInt();
 
+			// Salir del sistema
 			if (opcion == 2) {
 				break;
 			}
 
+			// Se asigna la catidad ingresada por el usuario
 			System.out.println("Introduce la cantidad:");
 			cantidad = sc.nextDouble();
 			cantidadActual = cantidad;
 
+			// Se calculan todos lo billetes
 			$100 = calcularBilletes$100();
 			$20 = calcularBilletes$20();
 			$10 = calcularBilletes$10();
@@ -47,6 +55,7 @@ public class Main {
 			$0_05 = calcularMoneda$0_05();
 			$0_01 = calcularMoneda$0_01();
 
+			// Se imprimen en pantalla los billetes
 			System.out.println("El minimo cambio de dolares para $" + cantidad + " es:");
 			System.out.println($100 + " billetes de cien - $100");
 			System.out.println($20 + " billetes de veinte - $20");
@@ -58,6 +67,7 @@ public class Main {
 			System.out.println($0_05 + " monedas de cinco - $0.05");
 			System.out.println($0_01 + " monedas de uno - $0.01");
 
+			// Se reinician los billetes para una nueva posible cantidad
 			$100 = 0;
 			$20 = 0;
 			$10 = 0;
@@ -72,75 +82,75 @@ public class Main {
 	}
 
 	private static int calcularMoneda$0_01() {
-		int $0_01 = (int) ((cantidadActual) / 0.01);
-		double restante = cantidadActual % 0.01;
+		int $0_01 = (int) ((cantidadActual) / 0.01); // Calculo la cantidad de monedas
+		double restante = cantidadActual % 0.01; // Calculo el restante para el siguiente metodo
 		cantidadActual = Math.round(restante * 100.00) / 100.00; // Redondeo para futuros metodos
-
+		
 		return $0_01;
 	}
 
 	private static int calcularMoneda$0_05() {
-		int $0_05 = (int) ((cantidadActual) / 0.05);
-		double restante = cantidadActual % 0.05;
+		int $0_05 = (int) ((cantidadActual) / 0.05); // Calculo la cantidad de monedas
+		double restante = cantidadActual % 0.05; // Calculo el restante para el siguiente metodo
 		cantidadActual = Math.round(restante * 100.00) / 100.00; // Redondeo para futuros metodos
-		System.out.println(cantidadActual);
+		
 		return $0_05;
 	}
 
 	private static int calcularMoneda$0_10() {
-		int $0_10 = (int) ((cantidadActual) / 0.10);
-		double restante = cantidadActual % 0.10;
+		int $0_10 = (int) ((cantidadActual) / 0.10); // Calculo la cantidad de monedas
+		double restante = cantidadActual % 0.10; // Calculo el restante para el siguiente metodo
 		cantidadActual = Math.round(restante * 100.00) / 100.00; // Redondeo para futuros metodos
-		System.out.println(cantidadActual);
+		
 		return $0_10;
 	}
 
 	private static int calcularMoneda$0_25() {
-		int $0_25 = (int) ((cantidadActual) / 0.25);
-		double restante = cantidadActual % 0.25;
+		int $0_25 = (int) ((cantidadActual) / 0.25); // Calculo la cantidad de monedas
+		double restante = cantidadActual % 0.25; // Calculo el restante para el siguiente metodo
 		cantidadActual = Math.round(restante * 100.00) / 100.00; // Redondeo para futuros metodos
-		System.out.println(cantidadActual);
+		
 		return $0_25;
 	}
 
 	private static int calcularBilletes$1() {
-		int $1 = (int) ((cantidadActual) / 1.0);
-		double restante = cantidadActual % 1.0;
+		int $1 = (int) ((cantidadActual) / 1.0); // Calculo la cantidad de billetes
+		double restante = cantidadActual % 1.0; // Calculo el restante para el siguiente metodo
 		cantidadActual = Math.round(restante * 100.00) / 100.00; // Redondeo para futuros metodos
-		System.out.println(cantidadActual);
+		
 		return $1;
 	}
 
 	private static int calcularBilletes$5() {
-		int $5 = (int) ((cantidadActual) / 5.0);
-		double restante = cantidadActual % 5.0;
+		int $5 = (int) ((cantidadActual) / 5.0); // Calculo la cantidad de billetes
+		double restante = cantidadActual % 5.0; // Calculo el restante para el siguiente metodo
 		cantidadActual = Math.round(restante * 100.00) / 100.00; // Redondeo para futuros metodos
-		System.out.println(cantidadActual);
+		
 		return $5;
 	}
 
 	private static int calcularBilletes$10() {
-		int $10 = (int) ((cantidadActual) / 10.0);
-		double restante = cantidadActual % 10.0;
+		int $10 = (int) ((cantidadActual) / 10.0); // Calculo la cantidad de billetes
+		double restante = cantidadActual % 10.0; // Calculo el restante para el siguiente metodo
 		cantidadActual = Math.round(restante * 100.00) / 100.00; // Redondeo para futuros metodos
-		System.out.println(cantidadActual);
+		
 		return $10;
 	}
 
 	static int calcularBilletes$20() {
-		int $20 = (int) ((cantidadActual) / 20.0);
-		double restante = cantidadActual % 20.0;
+		int $20 = (int) ((cantidadActual) / 20.0); // Calculo la cantidad de billetes
+		double restante = cantidadActual % 20.0; // Calculo el restante para el siguiente metodo
 		cantidadActual = Math.round(restante * 100.00) / 100.00; // Redondeo para futuros metodos
-		System.out.println(cantidadActual);
+		
 		return $20;
 	}
 
 	static int calcularBilletes$100() {
-		int $100 = (int) ((cantidadActual) / 100.0);
-		double restante = cantidadActual % 100.0;
+		int $100 = (int) ((cantidadActual) / 100.0); // Calculo la cantidad de billetes
+		double restante = cantidadActual % 100.0; // Calculo el restante para el siguiente metodo
 		cantidadActual = Math.round(restante * 100.00) / 100.00; // Redondeo para futuros metodos
 
-		System.out.println(cantidadActual);
+		
 		return $100;
 	}
 }
