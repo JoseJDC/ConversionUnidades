8efebaca607cd9a2535c167549489b9c0f42bcb9
JoseJDC <93933615+JoseJDC@users.noreply.github.com>
2023-07-13 22:46:34 -0600

    Refactorizacion de codigo
    Los métodos de calculo de cada billete eran casi idénticos asique hice un método mas genérico que hace el mismo calculo pero asignándole el valor de cada billete cuando se llama calcularCantidad(valor).

--- src/com/hk/Main.java
+++ src/com/hk/Main.java
@@ -44,16 +44,27 @@ public class Main {
 			cantidad = sc.nextDouble();
 			cantidadActual = cantidad;
 
-			// Se calculan todos lo billetes
-			$100 = calcularBilletes$100();
-			$20 = calcularBilletes$20();
-			$10 = calcularBilletes$10();
-			$5 = calcularBilletes$5();
-			$1 = calcularBilletes$1();
-			$0_25 = calcularMoneda$0_25();
-			$0_10 = calcularMoneda$0_10();
-			$0_05 = calcularMoneda$0_05();
-			$0_01 = calcularMoneda$0_01();
+			// Se calculan todos lo billetes con metodos especificos (Logica antigua)
+			// $100 = calcularBilletes$100();
+			// $20 = calcularBilletes$20();
+			// $10 = calcularBilletes$10();
+			// $5 = calcularBilletes$5();
+			// $1 = calcularBilletes$1();
+			// $0_25 = calcularMoneda$0_25();
+			// $0_10 = calcularMoneda$0_10();
+			// $0_05 = calcularMoneda$0_05();
+			// $0_01 = calcularMoneda$0_01();
+
+			// Se calculan todos lo billetes con un solo metodo
+			$100 = calcularCantidad$$$(100);
+			$20 = calcularCantidad$$$(20);
+			$10 = calcularCantidad$$$(10);
+			$5 = calcularCantidad$$$(5);
+			$1 = calcularCantidad$$$(1);
+			$0_25 = calcularCantidad$$$(0.25);
+			$0_10 = calcularCantidad$$$(0.10);
+			$0_05 = calcularCantidad$$$(0.05);
+			$0_01 = calcularCantidad$$$(0.01);
 
 			// Se imprimen en pantalla los billetes
 			System.out.println("El minimo cambio de dolares para $" + cantidad + " es:");
@@ -81,11 +92,19 @@ public class Main {
 		sc.close();
 	}
 
+	private static int calcularCantidad$$$(double valor) {
+		int $$$ = (int) ((cantidadActual) / valor); // Calculo la cantidad de monedas
+		double restante = cantidadActual % valor; // Calculo el restante para el siguiente metodo
+		cantidadActual = Math.round(restante * 100.00) / 100.00; // Redondeo para futuros metodos
+
+		return $$$;
+	}
+
 	private static int calcularMoneda$0_01() {
 		int $0_01 = (int) ((cantidadActual) / 0.01); // Calculo la cantidad de monedas
 		double restante = cantidadActual % 0.01; // Calculo el restante para el siguiente metodo
 		cantidadActual = Math.round(restante * 100.00) / 100.00; // Redondeo para futuros metodos
-		
+
 		return $0_01;
 	}
 
@@ -93,7 +112,7 @@ public class Main {
 		int $0_05 = (int) ((cantidadActual) / 0.05); // Calculo la cantidad de monedas
 		double restante = cantidadActual % 0.05; // Calculo el restante para el siguiente metodo
 		cantidadActual = Math.round(restante * 100.00) / 100.00; // Redondeo para futuros metodos
-		
+
 		return $0_05;
 	}
 
@@ -101,7 +120,7 @@ public class Main {
 		int $0_10 = (int) ((cantidadActual) / 0.10); // Calculo la cantidad de monedas
 		double restante = cantidadActual % 0.10; // Calculo el restante para el siguiente metodo
 		cantidadActual = Math.round(restante * 100.00) / 100.00; // Redondeo para futuros metodos
-		
+
 		return $0_10;
 	}
 
@@ -109,7 +128,7 @@ public class Main {
 		int $0_25 = (int) ((cantidadActual) / 0.25); // Calculo la cantidad de monedas
 		double restante = cantidadActual % 0.25; // Calculo el restante para el siguiente metodo
 		cantidadActual = Math.round(restante * 100.00) / 100.00; // Redondeo para futuros metodos
-		
+
 		return $0_25;
 	}
 
@@ -117,7 +136,7 @@ public class Main {
 		int $1 = (int) ((cantidadActual) / 1.0); // Calculo la cantidad de billetes
 		double restante = cantidadActual % 1.0; // Calculo el restante para el siguiente metodo
 		cantidadActual = Math.round(restante * 100.00) / 100.00; // Redondeo para futuros metodos
-		
+
 		return $1;
 	}
 
@@ -125,7 +144,7 @@ public class Main {
 		int $5 = (int) ((cantidadActual) / 5.0); // Calculo la cantidad de billetes
 		double restante = cantidadActual % 5.0; // Calculo el restante para el siguiente metodo
 		cantidadActual = Math.round(restante * 100.00) / 100.00; // Redondeo para futuros metodos
-		
+
 		return $5;
 	}
 
@@ -133,24 +152,23 @@ public class Main {
 		int $10 = (int) ((cantidadActual) / 10.0); // Calculo la cantidad de billetes
 		double restante = cantidadActual % 10.0; // Calculo el restante para el siguiente metodo
 		cantidadActual = Math.round(restante * 100.00) / 100.00; // Redondeo para futuros metodos
-		
+
 		return $10;
 	}
 
-	static int calcularBilletes$20() {
+	private static int calcularBilletes$20() {
 		int $20 = (int) ((cantidadActual) / 20.0); // Calculo la cantidad de billetes
 		double restante = cantidadActual % 20.0; // Calculo el restante para el siguiente metodo
 		cantidadActual = Math.round(restante * 100.00) / 100.00; // Redondeo para futuros metodos
-		
+
 		return $20;
 	}
 
-	static int calcularBilletes$100() {
+	private static int calcularBilletes$100() {
 		int $100 = (int) ((cantidadActual) / 100.0); // Calculo la cantidad de billetes
 		double restante = cantidadActual % 100.0; // Calculo el restante para el siguiente metodo
 		cantidadActual = Math.round(restante * 100.00) / 100.00; // Redondeo para futuros metodos
 
-		
 		return $100;
 	}
 }
